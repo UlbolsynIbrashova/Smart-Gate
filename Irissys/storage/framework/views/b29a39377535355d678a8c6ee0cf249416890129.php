a561876d7eec18acceb362b01ab27c56bf748975
UlbolsynIbrashova <56947580+UlbolsynIbrashova@users.noreply.github.com>
2021-05-19 17:19:51 +0600

    Add files via upload

---
+++ Irissys/storage/framework/views/b29a39377535355d678a8c6ee0cf249416890129.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\gaira\Desktop\Open Server (Базовый, Расширенный, Максимальный) 5.3.0\OSPanel\domains\irissys\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
